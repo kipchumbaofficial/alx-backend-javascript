ace1534dca424c1bfc2fa8c111d6bf3cd7a1278c
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-07-26 13:20:19 +0300

    Task 5: Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    this._checker();
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  _checker() {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function' || this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Must be overriden when extending');
+  }
+}
