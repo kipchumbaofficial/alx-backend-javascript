7d8ff78f22c63ad0059633377670295a3eefa988
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-07-26 16:52:51 +0300

    Task 7: Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  set name(newName) {
+    if (typeof newName == 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string')
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode == 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object  ${this.code}]`
+  }
+}
